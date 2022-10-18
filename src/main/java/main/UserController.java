ffceb91e8ec8deb51adb4ff64685a5b4616eef18
jomingee <jsh00999@naver.com>
2022-10-18 20:58:22 +0900

    프로젝트 공유

---
+++ src/main/java/main/UserController.java
@@ -0,0 +1,81 @@
+package main;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/userController/*")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	UserDAO userDAO;
+	
+	
+    public UserController() {
+    }
+    
+    
+    public void init() throws ServletException{
+    	userDAO = new UserDAO();
+    }
+
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request,response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request,response);
+	}
+	
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String nextPage=null;
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		String action = request.getPathInfo();
+		System.out.println("action:" + action);
+		
+		if(action.equals("/addUser.do")) {
+			String id=request.getParameter("id");
+			String password=request.getParameter("password");
+			String nickname=request.getParameter("nickname");
+			String phone_number=request.getParameter("phone_number");
+			
+			UserVO userVO = new UserVO(id, password, nickname, phone_number);
+			userDAO.addMember(userVO);
+			nextPage="../index/main.jsp";
+			
+		}else if(action.equals("/login.do")) {
+			String id=request.getParameter("id");
+			String password=request.getParameter("password");
+			
+			int loginResult=userDAO.login(id, password);
+			
+			if(loginResult==1) {
+				request.setAttribute("loginResult", loginResult);
+				HttpSession session = request.getSession();
+				session.setAttribute("sessionID", id);
+				nextPage="../index/main.jsp";
+			}else {
+				request.setAttribute("loginResult", loginResult);
+				nextPage="../login/loginFrom.jsp";
+			}
+		}
+		
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		dispatch.forward(request, response);
+		
+	}
+	
+	
+
+}
